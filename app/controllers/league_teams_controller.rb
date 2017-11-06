ea08aa4f6411446798514b8c36bbb1d565d86d00
David Janas <davidjanasr@gmail.com>
2017-11-06 16:35:00 -0500

    debugging

--- app/controllers/league_teams_controller.rb
+++ app/controllers/league_teams_controller.rb
@@ -3,14 +3,17 @@ class LeagueTeamsController < ApplicationController
   def create
 
     print "PARAMS"
-    print   params[:leagueTeamInfoList]
+    print params[:leagueTeamInfoList]
     params[:leagueTeamInfoList].each do |team|
+      team_hash = team.to_h
+      print "TEAM_HASH"
+      print team_hash
       LeagueTeam.create(
-        abbrName: team.abbrName,
-        cityName: team.cityName,
-        displayName: team.displayName,
-        divName: team.divName,
-        userName: team.userName
+        abbrName: team_hash.abbrName,
+        cityName: team_hash.cityName,
+        displayName: team_hash.displayName,
+        divName: team_hash.divName,
+        userName: team_hash.userName
       )
     end
   end
