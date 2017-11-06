601f29fd5c778e6d5ccbe55543f68982ece3aef9
David Janas <davidjanasr@gmail.com>
2017-11-06 15:08:07 -0500

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   resource :session, only: [:new, :create, :destroy]
 
-  post '/import/:platform/:leagueId/leagueteams', to: 'leagueteams#create'
+  post '/import/:platform/:leagueId/leagueteams', to: 'leagueTeams#create'
 end
